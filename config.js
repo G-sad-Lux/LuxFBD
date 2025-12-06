051c0fb8510a90d2413d4b8c749abc0df944a3d4
Patricio-Avila <57376227+Patricio-Avila@users.noreply.github.com>
2025-12-05 19:48:01 -0600

    Allow and add config.js for GH Pages

---
+++ config.js
@@ -0,0 +1,4 @@
+window.supabaseConfig = {
+    url: 'https://nsrduorngzyoiwpbkgku.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zcmR1b3JuZ3p5b2l3cGJrZ2t1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5MzI2NjUsImV4cCI6MjA4MDUwODY2NX0.eec9APoAe1a5ObsBQ6sWIIdjY-pWdD4s4Tt5oEWE5XY'
+};
